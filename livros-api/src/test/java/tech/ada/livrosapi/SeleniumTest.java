d2602495351925ea563f52d9f4c0502fcff98995
Felipe Zanardo <felipebueno6@hotmail.com>
2023-05-17 10:08:10 -0300

    Incluido teste de cadastro, edição e remoção com o Selenium

--- livros-api/src/test/java/tech/ada/livrosapi/SeleniumTest.java
+++ livros-api/src/test/java/tech/ada/livrosapi/SeleniumTest.java
@@ -9,7 +9,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SeleniumTest {
     private static WebDriver webDriver;
@@ -24,13 +26,85 @@ public class SeleniumTest {
 
     @AfterEach
     void tearDown() {
-        //webDriver.quit();
+        webDriver.quit();
     }
 
     @Test
-    void clicarBotaoCadatrarDeveRedirecionarParaPaginaDeCadastroTest() {
-        WebElement btnCadastrar = webDriver.findElement(By.xpath("/html/body/div[1]/nav/div/ul/li[2]/a"));
+    void clicarBotaoCadatrarDeveDirecionarParaPaginaDeCadastroSeleniumTest() {
+        WebElement btnCadastrar = webDriver.findElement(By.linkText("Cadastrar Livro"));
         btnCadastrar.click();
         assertEquals("http://localhost:8080/livros/cadastrar", webDriver.getCurrentUrl());
+    }
+
+    @Test
+    void cadastroDeLivroSeleniumTest() {
+        WebElement btnCadastrar = webDriver.findElement(By.linkText("Cadastrar Livro"));
+        btnCadastrar.click(); //entra na página de cadastro
+
+        webDriver.findElement(By.id("titulo")).sendKeys("Título 1");
+        webDriver.findElement(By.id("resumo")).sendKeys("Resumo 1");
+        webDriver.findElement(By.id("sumario")).sendKeys("Sumário 1");
+        webDriver.findElement(By.id("preco")).sendKeys("25,00");
+        webDriver.findElement(By.id("numeroPaginas")).sendKeys("500");
+        webDriver.findElement(By.id("isbn")).sendKeys("5451216565");
+        webDriver.findElement(By.id("dataPublicacao")).sendKeys("02/05/2025");
+
+        webDriver.findElement(By.xpath("/html/body/form/div[8]/button")).click(); //botão Criar
+
+        List<WebElement> titulosCadastrados = webDriver.findElements(By
+                .xpath("/html/body/div[2]/div/table/tbody/tr[*]/td[1]"));
+        assertFalse(titulosCadastrados.isEmpty()); //verifica se há itens cadastrados
+
+        List<WebElement> webElements = titulosCadastrados
+                .stream()
+                .filter(webElement -> webElement.getText().equals("Título 1"))
+                .toList();
+
+        assertFalse(webElements.isEmpty()); //verifica se o Título 1 foi cadastrado
+        assertEquals("http://localhost:8080/livros", webDriver.getCurrentUrl()); //verifica se retorna para página inicial
+
+    }
+
+    @Test
+    void remocaoDeLivroCadastradoSeleniumTest() {
+        cadastroDeLivroSeleniumTest();  //faz o cadastro para poder fazer o teste de remoção
+        List<WebElement> titulosCadastrados = webDriver
+                .findElements(By.xpath("/html/body/div[2]/div/table/tbody/tr[*]/td[1]"));
+
+        webDriver.findElement(By
+                .xpath("/html/body/div[2]/div/table/tbody/tr[1]/td[9]/form/button")).click(); //remove o primeiro item cadastrado
+        List<WebElement> titulosCadastradosPosRemocao = webDriver
+                .findElements(By.xpath("/html/body/div[2]/div/table/tbody/tr[*]/td[1]"));
+
+        assertEquals(titulosCadastrados.size() - 1, titulosCadastradosPosRemocao.size()); //verifica se um item foi removido
+    }
+
+    @Test
+    void clicarBotaoEditarDeveDirecionarParaPaginaDeEdicaoSeleniumTest() {
+        cadastroDeLivroSeleniumTest();  //faz o cadastro para poder fazer o teste de edição
+        webDriver.findElement(By
+                .xpath("/html/body/div[2]/div/table/tbody/tr[1]/td[8]/form/button")).click(); //clica no botão Editar
+
+        assertTrue(webDriver.getCurrentUrl().contains("http://localhost:8080/livros/editar/")); //verifica se direciona para página de edição
+    }
+
+    @Test
+    void editarLivroCadastradoSeleniumTest() {
+        clicarBotaoEditarDeveDirecionarParaPaginaDeEdicaoSeleniumTest(); //faz o cadastro e testa se está na página de edição
+        webDriver.findElement(By.id("titulo")).sendKeys("Título modificado");   //atualiza o nome do título
+        webDriver.findElement(By.xpath("/html/body/form/div[8]/button")).click(); //botão Editar
+
+        assertEquals("http://localhost:8080/livros", webDriver.getCurrentUrl()); //verifica se retorna para página inicial
+
+        List<WebElement> titulosCadastrados = webDriver
+                .findElements(By.xpath("/html/body/div[2]/div/table/tbody/tr[*]/td[1]"));
+
+        List<WebElement> webElements = titulosCadastrados
+                .stream()
+                .filter(webElement -> webElement.getText().equals("Título modificado"))
+                .toList();
+
+        assertTrue(webElements.isEmpty()); //verifica se o Título foi modificado
+
     }
 }
